f4cb3c5a69e6866089f7445ea52759ac2eadf54c
Eva Naranjo <evita.naranjo@gmail.com>
2021-12-14 10:44:47 +0000

    init proyect

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const root = document.querySelector("#root");
+  root.innerHTML = "";
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let combination = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        combination.push(pronoun[i] + adj[j] + noun[k]);
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+      }
+    }
+  }
+  let generator = "";
+  for (let i = 0; i < combination.length; i++) {
+    generator = generator + `${combination[i]}.com </br>`;
+  }
+  root.innerHTML = generator;
+  console.log(root);
 };
